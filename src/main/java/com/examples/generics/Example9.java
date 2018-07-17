19fd19d41b2809178bf761462cd2a48ed8e69890
gleisoncs <gleisoncs@gmail.com>
2018-07-17 07:39:08 +0100

    simple fix

--- src/main/java/com/examples/generics/Example9.java
+++ src/main/java/com/examples/generics/Example9.java
@@ -2,34 +2,19 @@ package com.examples.generics;
 
 public class Example9 {
 
-	public static <E> void printArray(E[] inputArray) {
-		
-		// create an array of strings
-		String[] strings = new String[10];
-
-		// cast it to an array of objects
-		Object[] objects = strings;
-
-		// insert an object into the array
-		objects[0] = new Object();
-		
-		//for (E element : inputArray)
-			//System.out.printf("%s ", element);
-	}
-
 	public static void main(String args[]) {
-		// Create arrays of Integer, Double and Character
 		Integer[] intArray = { 1, 2, 3, 4, 5 };
 		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
 		Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
 
-		System.out.println("Array integerArray contains:");
-		printArray(intArray); // pass an Integer array
+		printArray(intArray);
+		printArray(doubleArray);
+		printArray(charArray);
+	}
 
-		System.out.println("\nArray doubleArray contains:");
-		printArray(doubleArray); // pass a Double array
-
-		System.out.println("\nArray characterArray contains:");
-		printArray(charArray); // pass a Character array
+	public static <E> void printArray(E[] inputArray) {
+		for (E element : inputArray)
+			System.out.printf("%s ", element);
+		System.out.println();
 	}
 }
