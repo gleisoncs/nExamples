6542a312a417abaa482f09a6b0a53c49b9ca133d
gleisoncs <gleisoncs@gmail.com>
2019-02-19 22:28:50 +0000

    new files

--- src/main/java/com/examples/others/PatternMatcher.java
+++ src/main/java/com/examples/others/PatternMatcher.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class PatternMatcher {
 
 	public static void main(String[] args) {
-		String ms4 = "Aug 16 07:19:53 pnjsrchrec08.barnesandnoble.comLogfileMonitor|1502882393|pnjsrchrec08|critical|Logfile|OS|openView-6800.log|Wed Aug 16 07:15:01 2017|critical|Tomcat is not responsive; Solr service will be affected. Restart Solr if the problem persists.|LogfileMonitor";
+		String ms4 = "Aug 16 07:19:53 server1.barnesandnoble.comLogfileMonitor|15028855393|server1|critical|Logfile|OS|openView-6800.log|Wed Aug 16 07:19:01 2017|critical|Tomcat is not responsive; Solr service will be affected. Restart Solr if the problem persists.|LogfileMonitor";
 		// Pattern entityPattern4 =
 		// Pattern.compile("^\\w+\\s+\\d+\\s+\\d+:\\d+:\\d+\\s+\\S+\\s+\\w+\\|\\d+\\|(\\w+)\\|");
 		Pattern entityPattern4 = Pattern.compile("^\\w+\\s+\\d+\\s+\\S+\\s+\\w+\\.+\\w+\\.+\\w+\\|+\\d+\\|+(\\w+)");
